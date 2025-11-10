cfb13e4df5b24a8fe21ac6f21902a22c323ddaa2
F4RAN <parsa.hassani30@gmail.com>
2025-11-10 19:51:19 +0330

    fix(mongo): Sync the MongoUserRepository with real mongo functions

--- node/infrastructure/MongoUserRepository.ts
+++ node/infrastructure/MongoUserRepository.ts
@@ -1,15 +1,18 @@
+import { Db } from "mongodb";
 import { UserRepository } from "../application/ports/UserRepository";
 import ID from "../domain/user/ID";
 import User from "../domain/user/User";
 
 export class MongoUserRepository implements UserRepository{
-    constructor(private mongo){
-        this.mongo = mongo
+    private collection: any;
+    constructor(public db: Db){
+        this.db = db
+        this.collection = db.collection('users')
     }
-    async save(user: User){
-        await this.mongo.user.create(user)
+    async save(user: User): Promise<void>{
+        await this.collection.insertOne(user)
     }
     async findById(id: ID): Promise<User | null> {
-        return this.mongo.user.findById(id)
+        return this.collection.findOne(id.getValue())
     }
 }
